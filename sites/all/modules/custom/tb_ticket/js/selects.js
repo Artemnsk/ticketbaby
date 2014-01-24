09541186d1fd907ce1ed7b1473861cae32420e4e
Elizaveta Kabanova <iiiameye@gmail.com>
2014-01-24 14:40:29 +0700

    select and choose file

--- sites/all/modules/custom/tb_ticket/js/selects.js
+++ sites/all/modules/custom/tb_ticket/js/selects.js
@@ -3,7 +3,7 @@
         var $selects = $('#edit-field-ticket-category-und');
 						
 	$selects.easyDropDown({
-		cutOff: 10,
+		cutOff: 14,
 		wrapperClass: 'tickets-dropdown',
 		/*onChange: function(selected){
 			// do something
