1b87d97ff7df9e67dd3da6831c09d3a003447752
artemnsk <shelkov1991@gmail.com>
2014-02-01 13:20:13 +0700

    location field and its implementation on node/add/ticket page

--- sites/all/modules/custom/tb_ticket/js/selects.js
+++ sites/all/modules/custom/tb_ticket/js/selects.js
@@ -1,8 +1,8 @@
 (function($) {
     $(document).ready(function(){
-        var $selects = $('#edit-field-ticket-category-und');
-						
-	$selects.easyDropDown({
+        var selects = $('#edit-field-ticket-category-und, #edit-field-location-where-ticket-was-und');
+        
+	selects.easyDropDown({
 		cutOff: 14,
 		wrapperClass: 'tickets-dropdown',
 		/*onChange: function(selected){
