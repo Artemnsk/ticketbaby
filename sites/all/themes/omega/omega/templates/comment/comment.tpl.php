63d75f54583ac064fdf19b4bb2c70bf18997b7d6
Elizaveta Kabanova <iiiameye@gmail.com>
2014-02-02 23:24:57 +0700

    responds

--- sites/all/themes/omega/omega/templates/comment/comment.tpl.php
+++ sites/all/themes/omega/omega/templates/comment/comment.tpl.php
@@ -60,31 +60,34 @@
  */
 ?>
 <article<?php print $attributes; ?>>
-  <?php print render($title_prefix); ?>
-  <?php if ($new): ?>
-    <mark class="new"><?php print $new; ?></mark>
-  <?php endif; ?>
-  <h3<?php print $title_attributes; ?>><?php print $title; ?></h3>
-  <?php print render($title_suffix); ?>
-
-  <footer class="comment__info">
-    <?php print $user_picture; ?>
-    <p class="submitted comment__submitted"><?php print $submitted; ?></p>
-    <?php print $permalink; ?>
-  </footer>
-
-  <div<?php print $content_attributes; ?>>
-    <?php
-      // We hide the links now so that we can render them later.
-      hide($content['links']);
-      print render($content);
-    ?>
-    <?php if ($signature): ?>
-    <div class="user-signature comment__signature">
-      <?php print $signature; ?>
-    </div>
+    <div class="comment_left_wrapper">
+    <?php print render($title_prefix); ?>
+    <?php if ($new): ?>
+        <mark class="new"><?php print $new; ?></mark>
     <?php endif; ?>
-  </div>
+        <h3<?php print $title_attributes; ?>><?php print $title; ?></h3>
+        <?php print render($title_suffix); ?>
 
-  <?php print render($content['links']) ?>
+        <footer class="comment__info">
+            <?php print $user_picture; ?>
+            <p class="submitted comment__submitted"><?php print $submitted; ?></p>
+            <?php print $permalink; ?>
+        </footer>
+    </div>
+    <div class="comment_right_wrapper">
+        <div<?php print $content_attributes; ?>>
+            <?php
+            // We hide the links now so that we can render them later.
+            hide($content['links']);
+            print render($content);
+            ?>
+            <?php if ($signature): ?>
+                <div class="user-signature comment__signature">
+                    <?php print $signature; ?>
+                </div>
+            <?php endif; ?>
+        </div>
+
+        <?php print render($content['links']) ?>
+    </div>
 </article>
